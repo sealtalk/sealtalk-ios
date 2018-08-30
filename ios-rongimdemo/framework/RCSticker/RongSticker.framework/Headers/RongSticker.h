7f7fc1b1710bba60e9dd4fd2a6bae4239052894b
jenkins <jenkins@rongcloud.cn>
2018-08-30 17:34:41 +0800

    Release SealTalk Demo version 1.3.5

---
+++ ios-rongimdemo/framework/RCSticker/RongSticker.framework/Headers/RongSticker.h
@@ -0,0 +1,17 @@
+//
+//  RongSticker.h
+//  RongSticker
+//
+//  Created by Zhaoqianyu on 2018/8/7.
+//  Copyright © 2018年 RongCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RongSticker.
+FOUNDATION_EXPORT double RongStickerVersionNumber;
+
+//! Project version string for RongSticker.
+FOUNDATION_EXPORT const unsigned char RongStickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RongSticker/PublicHeader.h>
